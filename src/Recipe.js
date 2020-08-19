b7a70bdc600b7b66abc71cf14135671d8e6bd2ea
sanket <sanketgargate06@gmail.com>
2020-08-19 14:54:00 +0530

    added mapping for Recipe Component

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,17 @@
-import React from "react"
+import React from "react";
+import style from './recipe.module.css';
 
-const Recipe = ({title,calories,image}) => {
+const Recipe = ({title,calories,image,ingredients}) => {
     return(
-        <div>
-            <h1>{title}</h1>
-            <img src={image} alt=""/> 
+        <div className={style.recipe}>
+            <h1 className={style.title}>{title}</h1>
+            <img className={style.image} src={image} alt=""/>
             <p><b>Calories: </b>{calories}</p>
+            <ol>
+                {ingredients.map(ingredients => (
+                    <li className={style.ingredient}>{ingredients.text}</li>
+                ))}
+            </ol>
         </div>
     );
 }
